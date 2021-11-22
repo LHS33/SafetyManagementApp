8c63158c0a8bd66aea89050c21e99488c4139254
ysy <downtime8454@naver.com>
2021-11-22 16:11:28 +0900

    firebase login

--- app/src/main/java/com/example/safetymanagementapp/LoginActivity.java
+++ app/src/main/java/com/example/safetymanagementapp/LoginActivity.java
@@ -1,14 +1,85 @@
 package com.example.safetymanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnWorker;
+    Button btnManager;
+    Button btnLogin;
+    EditText editId;
+    EditText editPasswd;
+    private FirebaseAuth mAuth;
+
+    int Worker_Manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnWorker = findViewById(R.id.btnWorker);
+        btnManager = findViewById(R.id.btnManager);
+        btnLogin = findViewById(R.id.btnLogin);
+        editId = findViewById(R.id.editId);
+        editPasswd = findViewById(R.id.editPasswd);
+        mAuth = FirebaseAuth.getInstance();
+
+        //Worker == 0
+        //Manager == 1
+        Worker_Manager = 0;
+
+        btnWorker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Worker_Manager = 0;
+            }
+        });
+
+        btnManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Worker_Manager = 1;
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editId.getText().toString().trim();
+                String pwd = editPasswd.getText().toString().trim();
+
+                mAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if (Worker_Manager == 0) { //Worker
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else { //Manager
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        } else { //fail
+                            Toast.makeText(LoginActivity.this, "login fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
