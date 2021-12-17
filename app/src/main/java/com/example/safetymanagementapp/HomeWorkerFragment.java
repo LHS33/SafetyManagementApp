8559b51dcf33b50609d7532c94129c92bafe0044
soyoung12 <hena019@naver.com>
2021-12-17 15:40:55 +0900

    프로그레스바 수정 / 날씨 api( 완료x)

--- app/src/main/java/com/example/safetymanagementapp/HomeWorkerFragment.java
+++ app/src/main/java/com/example/safetymanagementapp/HomeWorkerFragment.java
@@ -1,11 +1,15 @@
 package com.example.safetymanagementapp;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +21,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 public class HomeWorkerFragment extends Fragment {
 
     View view;
@@ -26,6 +33,7 @@ public class HomeWorkerFragment extends Fragment {
     int moistureValue;
     int dustValue;
     TextView COValue;
+    TextView Today;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home_worker, container, false);
@@ -35,11 +43,89 @@ public class HomeWorkerFragment extends Fragment {
         dustProgressBar = view.findViewById(R.id.dustProgressBar);
         COProgressBar = view.findViewById(R.id.COProgressBar);
 
+        moistureProgressBar.setIndeterminate(false);
+        dustProgressBar.setIndeterminate(false);
+        COProgressBar.setIndeterminate(false);
+
+        Today  = view.findViewById(R.id.tVToday);
         COValue = view.findViewById(R.id.tVCOValue);
 
+        //현재시간 설정
+       Today.setText(getTime());
+
+        //데이터베이스 센서 값 받아오기
+
+
+        getSensorValue();
+
+
+        // URL 설정.
+        String service_key = "Yvgu9A%2BZAvc3h4ok1csvEzNN8mBLy3g0bj%2FB7uhTkGPbaQ49fnVIxR78irZKiokYcoTilrEgRiijbW9fa4r0lg%3D%3D";
+        String num_of_rows = "10";
+        String page_no = "1";
+        String date_type = "JSON";
+        String base_date = "2021210";
+        String base_time = "0600";
+        String nx = "55";
+        String ny = "127";
+
+        String url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?" +
+                "serviceKey="+service_key+
+                "&numOfRows="+num_of_rows+
+                "&pageNo="+page_no+
+                "&dataType="+date_type+
+                "&base_date="+base_date+
+                "&base_time="+base_time+
+                "&nx="+nx+
+                "&ny="+ny;
+
+        // AsyncTask를 통해 HttpURLConnection 수행.
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
 
         return view;
     }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpConnection requestHttpURLConnection = new RequestHttpConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            System.out.println("출력 값: "+s);
+            Log.d("onPostEx", "출력 값 : "+s);
+        }
+    }
+
+
+
+    private String getTime() {
+        long now = System.currentTimeMillis();
+    Date date = new Date(now);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd (E) HH시 mm분");
+    String getTime = dateFormat.format(date);
+    return getTime; }
+
 
     public void getSensorValue() {
         /*
@@ -53,34 +139,39 @@ public class HomeWorkerFragment extends Fragment {
         //저장시킬 노드 참조객체 가져오기
         DatabaseReference rootRef = firebaseDatabase.getReference().child("sensor").child("1-set"); // () 안에 아무것도 안 쓰면 최상위 노드드
 
+        DatabaseReference rootRef2 = firebaseDatabase.getReference().child("sensor").child("2-set");
+
         ChildEventListener mChildEventListener;
         mChildEventListener = new ChildEventListener() {
+
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String data = (String) snapshot.getValue(String.class);
+                String data = (String) snapshot.getValue().toString();
                 COValue.setText(data);
+               moistureProgressBar.setProgress((int) Double.parseDouble(data));
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String data = (String) snapshot.getValue().toString();
                 COValue.setText(data);
+                moistureProgressBar.setProgress((int) Double.parseDouble(data));
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                String data = (String) snapshot.getValue().toString();
-                //textView2.setText(data);
+
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String data = (String) snapshot.getValue().toString();
-                //textView2.setText(data);
+
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
 
             }
         };
