448b5daa18005d0009280a5c336f60857676e49b
downtime8454_desktop <downtime8454@naver.com>
2022-02-04 18:36:49 +0900

    자동 로그인, 아이디 저장, 로그아웃

--- app/src/main/java/com/example/safetymanagementapp/LoginActivity.java
+++ app/src/main/java/com/example/safetymanagementapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.safetymanagementapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -28,14 +29,18 @@ public class LoginActivity extends AppCompatActivity {
     EditText editPasswd;
 
     CheckBox cBAutoLogin;
+    CheckBox cBIDSave;
+    private  boolean autoLogin;
     private  boolean saveLoginData;
+    private String string_ID;
     private SharedPreferences appData;
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
 
-
     int Worker_Manager;
+
+    private long mBackWait = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         editPasswd = findViewById(R.id.editPasswd);
         mAuth = FirebaseAuth.getInstance();
         cBAutoLogin = findViewById(R.id.checkBoxAutoLogin);
+        cBIDSave = findViewById(R.id.checkBoxIDSave);
 
         //Worker == 0
         //Manager == 1
@@ -82,9 +88,8 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-
-
                             if (Worker_Manager == 0 | Worker_Manager == 1) { //Worker && Manager
+                                save();
                                 startActivity(intent);
                             }
 
@@ -99,14 +104,28 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        /*
+        cBAutoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save(); //이거 여기다가 말고 로그인버튼 누를때로 옮기기.
+            }
+        });
+        */
+
+        /*
+        //자동로그인 체크박스 클릭하면
         if(cBAutoLogin.isChecked()){
 
         }
+        */
     }
 
     public void onStart(){
         super.onStart();
-        if(saveLoginData){
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        load();
+        if(autoLogin){ //자동로그인 설정했으면 홈화면으로.
             currentUser = mAuth.getInstance().getCurrentUser();
             if(currentUser!=null){
                 String email = currentUser.getEmail();
@@ -122,10 +141,43 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         }
+        if(saveLoginData){ //아이디저장 설정했으면
+            editId.setText(string_ID);
+            cBIDSave.setChecked(saveLoginData);
+        }
     }
 
+    //설정값 저장하는 함수
+    //근무자, 관리자 버튼 선택 여부도 저장을 해야하나?
+    private void save() {
+        SharedPreferences.Editor editor = appData.edit();
+
+        editor.putBoolean("AUTO_LOGIN", cBAutoLogin.isChecked());
+        editor.putBoolean("SAVE_LOGIN_DATA", cBIDSave.isChecked());
+        if(cBIDSave.isChecked()){
+            editor.putString("ID", editId.getText().toString().trim());
+        }
+
+        editor.apply();
+    }
+
+    //설정값 불러오는 함수
     private void load(){
+        autoLogin = appData.getBoolean("AUTO_LOGIN", false);
         saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA", false);
+        string_ID = appData.getString("ID", "");
+    }
+
+    // 뒤로가기 두 번 누르면 종료
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis() - mBackWait > 2000){
+            mBackWait = System.currentTimeMillis();
+            Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        } else{
+            ActivityCompat.finishAffinity(this);
+            System.exit(0);
+        }
     }
 }
 
